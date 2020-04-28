855fa34e89abec23f487e10c19032ec9e4de273a
marlonchiu <Knight1898@outlook.com>
2020-04-28 21:06:55 +0800

    lesson4 通用代码提取封装

--- src/containers/TodoList/__tests__/unit/Header.spec.js
+++ src/containers/TodoList/__tests__/unit/Header.spec.js
@@ -1,5 +1,6 @@
 import { shallowMount } from '@vue/test-utils'
 import Header from '../../components/Header.vue'
+import { findTestWrapper } from '../../../../utils/testUtils'
 
 describe('Header.vue', () => {
   it('Header 样式改变 做提示', () => {
@@ -9,7 +10,7 @@ describe('Header.vue', () => {
 
   it('Header 包含 input 输入框', () => {
     const wrapper = shallowMount(Header)
-    const input = wrapper.find('[data-test="input"]')
+    const input = findTestWrapper(wrapper, 'input')
     // const textInput = wrapper.find('input[type="text"]')
     expect(input.exists()).toBe(true)
   })
@@ -22,7 +23,7 @@ describe('Header.vue', () => {
 
   it('Header 中 input 输入框发生变化，数据会随着改变', () => {
     const wrapper = shallowMount(Header)
-    const input = wrapper.find('[data-test="input"]')
+    const input = findTestWrapper(wrapper, 'input')
     input.setValue('some value')
     const inputValue = wrapper.vm.$data.inputValue
     expect(inputValue).toBe('some value')
@@ -30,7 +31,7 @@ describe('Header.vue', () => {
 
   it('Header 中 input 输入框输入回车 无内容时无反应', () => {
     const wrapper = shallowMount(Header)
-    const input = wrapper.find('[data-test="input"]')
+    const input = findTestWrapper(wrapper, 'input')
     input.setValue('')
     input.trigger('keyup.enter')
     expect(wrapper.emitted().add).toBeFalsy()
@@ -38,7 +39,7 @@ describe('Header.vue', () => {
 
   it('Header 中 input 输入框输入回车 有内容时触发事件，同时清空input', () => {
     const wrapper = shallowMount(Header)
-    const input = wrapper.find('[data-test="input"]')
+    const input = findTestWrapper(wrapper, 'input')
     input.setValue('some value')
     input.trigger('keyup.enter')
     expect(wrapper.emitted().add).toBeTruthy()
