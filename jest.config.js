1e5082fcf064d36b097f85523be5b486d7bfd051
marlonchiu <Knight1898@outlook.com>
2020-04-30 08:43:19 +0800

    4-13 CodeCoverage 代码覆盖率

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest',
+  collectCoverageFrom: ['**/*.{js,vue}', '!**/node_modules/**'],
   testMatch: [
     '**/__tests__/unit/**/*.(spec|test).{j,t}s?(x)'
   ]
